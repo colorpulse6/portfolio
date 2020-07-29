2e3ae389e72f0d23fe328eb89e27dd77fab534ec
Nic Barnes <colorpulse@gmail.com>
2020-07-29 18:36:00 +0200

    added success page to form

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Layout from "../components/layout"
 import profileImg from "../imgs/ProfilePic.jpeg"
 import indexStyles from "./index.module.scss"
 
+
 const IndexPage = () => {
 
   return (
@@ -15,6 +16,7 @@ const IndexPage = () => {
         <h2>I'm Nic, a full stack web developer. Welcome.</h2>
         
       </div>
+    
     </Layout>
   )
 

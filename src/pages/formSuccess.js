8b9da295461c6b84c5e7157a26d364eee9ff1f98
Nic Barnes <colorpulse@gmail.com>
2020-07-29 18:17:19 +0200

    added success page to form

---
+++ src/pages/formSuccess.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Contact = () => {
+    return (
+
+        <div>
+            Your message was successfull!
+        </div>
+
+    )}

95d487f3030a49cf8453a3289afca3c7671fc245
Nic Barnes <colorpulse@gmail.com>
2020-07-29 18:29:02 +0200

    added success page to form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,15 @@ const Contact = () => {
   return (
     <Layout>
       <h1>Contact</h1>
-      <form name="contact" method="POST" data-netlify="true" action="/formSuccess">
+      <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        action="/formSuccess"
+        netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="bot-field" />
+         <input type="hidden" name="form-name" value="contact" />
         <p>
           <label>
             Your Name: <input type="text" name="name" />
--- src/pages/formSuccess.js
+++ src/pages/formSuccess.js
@@ -2,12 +2,11 @@ import React from "react"
 import Layout from "../components/layout"
 
 const FormSuccess = () => {
-    return (
-        <Layout>
-            <div>
-                Your message was successfull!
-            </div>
-        </Layout>
-    )}
+  return (
+    <Layout>
+      <div>Your message was successfull!</div>
+    </Layout>
+  )
+}
 
-    export default FormSuccess
+export default FormSuccess

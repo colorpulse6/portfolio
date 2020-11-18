804074af6073c08ffb4447e2dd89c50c84c0f4c3
Nic Barnes <colorpulse@gmail.com>
2020-11-18 19:40:13 +0100

    switched order of projects

--- gatsby-node.js
+++ gatsby-node.js
@@ -44,9 +44,10 @@ module.exports.createPages = async ({ graphql, actions }) => {
 exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
   const projects = [
     { name: "Hoop.It.App", link: "https://hoopitapp.herokuapp.com/", imgSrc:'https://res.cloudinary.com/duzle7rzg/image/upload/q_auto,f_auto/v1596015873/Portfolio/project-images/czoivcwynjlfuoti0rlp.png' },
+    { name: "Fire Store", link: "https://fire-store.netlify.app/", imgSrc:'https://res.cloudinary.com/duzle7rzg/image/upload/v1605721753/Portfolio/project-images/jfru1jpn9wnpnecfcokp.png' },
     { name: "Gigzilla", link: "https://gig-zilla.herokuapp.com/", imgSrc:'https://res.cloudinary.com/duzle7rzg/image/upload/q_auto,f_auto/v1596016004/Portfolio/project-images/h5epboy4xkokpqp5p6aj.png' },
     { name: "Mad Science", link: "https://colorpulse6.github.io/mad-science/", imgSrc:'https://res.cloudinary.com/duzle7rzg/image/upload/q_auto,f_auto/v1596015872/Portfolio/project-images/s5xigxb6xn8ruluk6gq4.png' },
-    { name: "Fire Store", link: "https://fire-store.netlify.app/", imgSrc:'https://res.cloudinary.com/duzle7rzg/image/upload/v1605721753/Portfolio/project-images/jfru1jpn9wnpnecfcokp.png' },
+    
 
   ]
   projects.forEach(project => {

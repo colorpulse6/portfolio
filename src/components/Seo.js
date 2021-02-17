2692854c09036c81a817072429c55073b255c04f
Nic Barnes <colorpulse@gmail.com>
2021-02-17 23:06:04 +0100

    Added SEO

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -20,7 +20,7 @@ const SEO = ({ title, description, image, article }) => {
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
-    image: `${siteUrl}${image || defaultImage}`,
+    image:  defaultImage,
     url: `${siteUrl}${pathname}`,
   }
 

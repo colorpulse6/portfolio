8bb1901ed9790d2a7e30ab231905c0f034ca18cd
Nic Barnes <colorpulse@gmail.com>
2021-02-17 23:17:08 +0100

    Added SEO

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -39,7 +39,7 @@ const SEO = ({ title, description, image, article }) => {
         <meta property="og:description" content={seo.description} />
       )}
 
-      {seo.image && <meta property="og:image" content={seo.image} />}
+      {seo.image && <meta name="image" property="og:image" content={seo.image} />}
 
       <meta name="twitter:card" content="summary_large_image" />
 
